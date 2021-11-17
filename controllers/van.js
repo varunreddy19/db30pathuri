1f45bb7142edb166ac00120738b11cc03bf1e091
varunreddy19 <pvarunreddy48@gmail.com>
2021-11-17 15:48:52 -0600

    Added delete

--- controllers/van.js
+++ controllers/van.js
@@ -1,6 +1,6 @@
 var vans = require('../models/van'); 
  
-// List of all Flight
+// List of all vans
 
 exports.van_list = async function(req, res) { 
     try{ 
@@ -13,24 +13,56 @@ exports.van_list = async function(req, res) {
     }   
 };
  
-// for a specific Flight. 
-exports.van_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Van detail: ' + req.params.id); 
+// for a specific vans. 
+exports.van_detail = async function(req, res) { 
+    console.log("detail" + req.params.id)
+    try {
+        result = await vans.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    } 
 }; 
  
-// Handle Flight create on POST. 
+// Handle vans create on POST. 
 exports.van_create_post = function(req, res) { 
     res.send('NOT IMPLEMENTED: Van create POST'); 
 }; 
  
-// Handle Flight delete form on DELETE. 
-exports.van_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Van delete DELETE ' + req.params.id); 
-}; 
+// Handle vans delete form on DELETE. 
+exports.van_delete = async function(req, res) { 
+        console.log("delete "  + req.params.id) 
+        try { 
+            result = await vans.findByIdAndDelete( req.params.id) 
+            console.log("Removed " + result) 
+            res.send(result) 
+        } catch (err) { 
+            res.status(500) 
+            res.send(`{"error": Error deleting ${err}}`); 
+        } 
+    };  
  
-// Handle Flight update form on PUT. 
-exports.van_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Van update PUT' + req.params.id); 
+ 
+// Handle vans update form on PUT. 
+exports.van_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await vans.findById(req.params.id)
+            // Do updates of properties        
+        if (req.body.vanType)
+            toUpdate.vanType = req.body.vanType;
+        if (req.body.vanPrice)
+            toUpdate.vanPrice = req.body.vanPrice;
+        if (req.body.vanColor)
+            toUpdate.vanColor = req.body.vanColor;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
 };
 
 exports.van_view_all_Page = async function(req, res) { 
